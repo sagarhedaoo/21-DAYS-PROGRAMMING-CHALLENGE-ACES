8c393cf6cad43278f2b9e0afd30d888f2bc20e46
Sagar Hedaoo <sagarhedaoo@gmail.com>
2020-10-09 21:50:01 +0530

    Day 5- Done with some printing statements

---
+++ Day 5/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+//This is a Comment
+//STRINGS
+print("Hello World!!!")
+
+//Code in between string in called STRING INTERPOLATION
+print("Hello \(2+3) World!!!")
+print("Hello \(200*5) World!!!")
+print("The Result of 5 + 3 is \(5+3)")
+
+//VARIABLES
+
+var a = 200
+var b = 10
+print("value of a is \(a)")
+print(b)
+
+//ARRAYS
+
+var numbers = [10 ,20 , 30, 40]
+
+
